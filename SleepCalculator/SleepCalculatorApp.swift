b1d4fc2cb669b00dd8ead992a3979ac4365ef8bd
Dmaradan <diegomartin1313@gmail.com>
2023-10-04 20:44:33 +0200

    Basic UI

---
+++ SleepCalculator/SleepCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepCalculatorApp.swift
+//  SleepCalculator
+//
+//  Created by Diego Martin on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
