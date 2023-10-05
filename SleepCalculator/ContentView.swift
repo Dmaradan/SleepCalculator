5b018aafdb003719f3c2c82b85d80fd29d297d87
Dmaradan <diegomartin1313@gmail.com>
2023-10-05 16:54:27 +0200

    Show ideal bedtime all the time - remove calculate toolbar item

--- SleepCalculator/ContentView.swift
+++ SleepCalculator/ContentView.swift
@@ -16,6 +16,35 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    var calculatedBedtime: String {
+        var bedTime = "Please select all your input values"
+        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            
+            // unwrap values and convert to seconds
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            
+            // make prediction
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            bedTime = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+        }
+        
+        return bedTime
+    }
+    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
@@ -45,12 +74,14 @@ struct ContentView: View {
                         .font(.headline)
                     Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20, step: 1)
                 }
+                
+                Text("Your ideal bedtime is \(calculatedBedtime)")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
             }
             .padding()
             .navigationTitle("SleepCalculator")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
+           
             .alert(alertTitle, isPresented: $showingAlert) {
                 Button("Ok"){}
             } message: {
